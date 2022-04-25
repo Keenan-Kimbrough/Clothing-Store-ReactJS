8ed9f54918d505bd5ebb8af7072e8f6983d2cb53
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-04-25 11:33:09 -0400

    seperated components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,19 @@
+
+import CategoryItem from '../cateogory-item/category-item.component'
+import './directory.styles.scss'
+const Directory = ({categories}) =>{
+
+    return (
+
+        <div>
+            <div className="directory-container">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category}/>
+        ))}
+      </div>
+    
+        </div>
+    )
+}
+
+export default Directory
