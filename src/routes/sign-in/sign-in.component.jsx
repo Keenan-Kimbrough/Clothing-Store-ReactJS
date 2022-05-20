9c31ab93ea9788196dc9b2cd48eb4b5f5ea60233
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-05-19 21:32:44 -0400

    created email form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import { useInsertionEffect} from 'react'
 import { getRedirectResult } from 'firebase/auth'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
 
 
 import {signInGooglePopup,createUserDocumentFromAuth, signInWithGoogleRedirect} from '../../utils/firebase/firebase.utils'
@@ -15,7 +16,7 @@ const SignIn = () => {
         <div>
             <h1> Sign In page</h1>
            <button onClick={logGoogleUser}>Sign in with Google Prompt</button>
-           
+           <SignUpForm/>
         </div>
     )
 }
